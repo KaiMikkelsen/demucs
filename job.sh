3a0100ef39c40f55d4dc5a427766f17586a8711a
KaiMikkelsen <squabee@gmail.com>
2024-10-09 17:50:59 -0400

    new env sctipt

--- job.sh
+++ job.sh
@@ -3,9 +3,9 @@
 #SBATCH --cpus-per-task=6  # Refer to cluster's documentation for the right CPU/GPU ratio
 #SBATCH --mem=32000M       # Memory proportional to GPUs: 32000 Cedar, 47000 Béluga, 64000 Graham.
 
-source ../demucs_env/bin/activate
+source ../ENV/bin/activate
 
-demucs test.mp3 --out job_outputs
+demucs run_separation_demucs.sh
 
 # ls -d $SLURM_TMPDIR
 
