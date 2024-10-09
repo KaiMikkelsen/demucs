4645d6e225acde5c5032cbb924391d1c3f224fa8
KaiMikkelsen <squabee@gmail.com>
2024-10-09 18:00:45 -0400

    sep

--- run_separation_demucs.sh
+++ run_separation_demucs.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Define the path where the song files are located
-data_path="../data/MUSDB18-7/test"
+data_path="../data/MUSDB18/test"
+print(f"separating tracks located in {data_path}")
 
 # Iterate through each .mp3 file in the data_path directory
 for song_file in "$data_path"/*.mp4; do
