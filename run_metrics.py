8fa8ed06ce73ed4dd742f129b0b89b68ca77510b
KaiMikkelsen <squabee@gmail.com>
2024-10-09 17:26:14 -0400

    better scripts

---
+++ run_metrics.py
@@ -0,0 +1,35 @@
+import musdb
+import museval
+
+# # initiate musdb
+# mus = musdb.DB(root="/Users/kaimikkelsen/canada_compute/data/MUSDB18-7")
+
+# # evaluate an existing estimate folder with wav files
+# scores = museval.eval_mus_dir(
+#     dataset=mus,  # instance of musdb
+#     estimates_dir="/Users/kaimikkelsen/canada_compute/demucs/separated/htdemucs",  # path to estimate folder
+#     output_dir='/Users/kaimikkelsen/canada_compute/demucs/results',  # set a folder to write eval json files
+#     ext='wav'
+# )
+
+# #args = parser.parse_args()
+# method = museval.EvalStore(frames_agg="median", tracks_agg="median")
+# method.add_eval_dir("/Users/kaimikkelsen/canada_compute/demucs/results")
+# print(method)
+
+# initiate musdb
+mus = musdb.DB(root="../data/MUSDB18-7")
+
+# evaluate an existing estimate folder with wav files
+scores = museval.eval_mus_dir(
+    dataset=mus,  # instance of musdb
+    estimates_dir="/separated/htdemucs",  # path to estimate folder
+    output_dir='/results',  # set a folder to write eval json files
+    ext='wav'
+)
+
+#args = parser.parse_args()
+method = museval.EvalStore(frames_agg="median", tracks_agg="median")
+method.add_eval_dir('/Users/kaimikkelsen/canada_compute/demucs/results')
+print(method)
+
---
+++ run_separation_demucs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Define the path where the song files are located
+data_path="../data/MUSDB18-7/test"
+
+# Iterate through each .mp3 file in the data_path directory
+for song_file in "$data_path"/*.mp4; do
+  # Call demucs on each song file
+  echo "$song_file"
+  demucs "$song_file"
+done
